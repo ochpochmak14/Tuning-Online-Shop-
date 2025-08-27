05a5fe13c1e9079ce4a3b565887dded82fe8b9b2
Emin <makhmudovemin441@gmail.com>
2025-08-27 16:13:22 +0500

    log in function end 1.0

--- tuning_shop/users/views.py
+++ tuning_shop/users/views.py
@@ -6,7 +6,7 @@ from .forms import UserRegisterForm
 
 # Create your views here.
 
-def reister(request):
+def register(request):
     if request.method == "POST":
         form = UserRegisterForm(request.POST)
         if form.is_valid():
