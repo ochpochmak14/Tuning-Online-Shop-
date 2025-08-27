aa0d2b1fd90d541ec44e4ca0e8e3e099fa11740f
Emin <makhmudovemin441@gmail.com>
2025-08-27 14:14:52 +0500

    don't finished

---
+++ tuning_shop/users/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include
+from users import views as user_views
+from django.shortcuts import redirect
+
+
+def redirect_to_login(request):
+    return redirect('login')
+
+url_patterns = [
+    path("register/", user_views.register, name='register')
+]
--- tuning_shop/users/views.py
+++ tuning_shop/users/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm
+
 
 # Create your views here.
+
+def reister(request):
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data('username')
+            messages.success(request, f"Аккаунт {username} успешно создан! Добро пожаловать в M-Performance Hub — переходите в каталог и выберите тюнинг для своего BMW!")
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return redirect("/")
+
+@login_required
+def profile(request):
+    return redirect("/")       
