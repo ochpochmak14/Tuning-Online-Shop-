8bb08b0b63c88e36b67f82bc4ac2b7ea5ce4df83
Emin <makhmudovemin441@gmail.com>
2025-08-27 13:32:07 +0500

    created apps, главная страница с navbarom

--- tuning_shop/tuning_shop/settings.py
+++ tuning_shop/tuning_shop/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'products',
+    'orders',
+    'users',
+    'cart',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +59,7 @@ ROOT_URLCONF = 'tuning_shop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
