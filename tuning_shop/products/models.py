6e88574dcd84bfa3f470dfdbad89aa5d19f3715f
Emin <makhmudovemin441@gmail.com>
2025-08-29 17:12:20 +0500

    1.1

--- tuning_shop/products/models.py
+++ tuning_shop/products/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Brands(models.Model):
+    brand = models.CharField()
+    logo = models.ImageField()
+    
 class Products(models.Model):
     pass
+
+
     
